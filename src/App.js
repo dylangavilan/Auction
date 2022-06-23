c2127170b733e298d65a24757c603c740f4f6810
dylangavilan <dylan.gavilan32@gmail.com>
2022-06-23 11:38:09 -0300

    add

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ import { ERC721 } from "./utils/erc721";
 function App() {
   const networkId = 4;
   const tokenId = 33;
-  const nftAddress = "0x5ddd592791d0c2260d6105879c1ff17ad74e1d42 ";
   const contractAddress = "0x3feaf4c06211680e5969a86adb1423fc8ad9e994";
+  const nftAddress = "0x5ddd592791d0c2260d6105879c1ff17ad74e1d42";
   const [wallet, setWallet] = useState("");
   const [highestBid, sethighestBid] = useState();
   const [reservePrice, setReservePrice] = useState();
